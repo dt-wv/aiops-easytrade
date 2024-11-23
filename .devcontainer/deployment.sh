696366dd59a3d1c6eb7e6f3fa87e7eee98f4cfd5
Wim Verhaeghe <wim.verhaeghe@dynatrace.com>
2024-11-23 10:21:36 +0100

    initial commit

---
+++ .devcontainer/deployment.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+########################## 
+# istio setup
+export PATH=$PWD/istio-1.24/bin:$PATH
+istioctl install -f istio/istio-operator.yaml --skip-confirmation
+
+sleep 30 
+
+##########################
+# update istio ingress
+kubectl patch svc -n istio-system istio-ingressgateway --patch "$(cat istio/patch.yaml)"
+kubectl delete pod --all -n istio-system
+
+##########################
+# create easytrade the namespace
+kubectl create namespace easytrade
+
+# then use the manifests to deploy
+kubectl -n easytrade apply -f easytrade-k8s-manifests
+
+# Optional: if you want the problem patterns to be automatically
+# enabled once a day, deploy these manifests too
+kubectl -n easytrade apply -f easytrade-k8s-manifests/problem-patterns
+kubectl label namespace easytrade istio-injection=enabled
