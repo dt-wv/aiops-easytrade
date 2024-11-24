083f3f59c528508157caa3cc5e460b70afd254fd
Wim Verhaeghe <wim.verhaeghe@dynatrace.com>
2024-11-24 15:42:56 +0100

    update

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -31,14 +31,13 @@ read -p 'Tenant ID: ' TENANT_ID
 # Install Dynatrace
 kubectl create namespace dynatrace
 kubectl apply -f https://github.com/Dynatrace/dynatrace-operator/releases/download/v1.3.2/kubernetes.yaml
-
+kubectl -n dynatrace wait pod --for=condition=ready --selector=app.kubernetes.io/name=dynatrace-operator,app.kubernetes.io/component=webhook --timeout=120s
 kubectl -n dynatrace create secret generic dynakube --from-literal="apiToken=$OPERATOR_TOKEN" --from-literal="dataIngestToken=$DATA_INGEST_TOKEN"
 
 sed -i 's/REPLACE_TENANT_ID/$TENANT_ID/g' dynatrace/application.yaml
 
-sleep 20
 kubectl apply -f dynatrace/application.yaml
-sleep 30
+sleep 60
 
 ##########################
 # Install Easytrade
