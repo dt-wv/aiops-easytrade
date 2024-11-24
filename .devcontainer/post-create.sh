25b2ec838e14291f80f31309ce9812dbda3cbdfa
Wim Verhaeghe <wim.verhaeghe@dynatrace.com>
2024-11-24 14:40:37 +0100

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-read -p "Operator Token: " OPERATOR_TOKEN
-read -p "Data ingest Token: " DATA_INGEST_TOKEN
-read -p "Tenant ID: " TENANT_ID
-
 # Install
 kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 chmod +x .devcontainer/deployment.sh && source .devcontainer/deployment.sh
